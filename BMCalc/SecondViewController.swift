17d1e0aa35fe798d82243c6b3b0f217934a4962a
sritar99 <sadhutarun00@gmail.com>
2020-02-02 15:17:57 +0530

    Initial Commit

---
+++ BMCalc/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  BMCalc
+//
+//  Created by Sri Harsha on 02/02/20.
+//  Copyright © 2020 Sri Harsha. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
